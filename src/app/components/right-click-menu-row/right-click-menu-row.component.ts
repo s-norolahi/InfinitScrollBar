aca811e1584d76280909894f05e6fe309cfeb881
s.norollahi <sa.nourollahi@gmail.com>
2024-09-25 15:47:42 +0330

    rightClick with multi select with ctl click

--- src/app/components/right-click-menu-row/right-click-menu-row.component.ts
+++ src/app/components/right-click-menu-row/right-click-menu-row.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './right-click-menu-row.component.html',
   styleUrls: ['./right-click-menu-row.component.css']
 })
-export class RightClickMenuRowComponent
+/*export class RightClickMenuRowComponent
 {
 
   items: Item[] = [
@@ -43,4 +43,61 @@ interface Item
 {
   name: string;
   value: number;
+}
+
+
+-----------------multiselect*/
+export class RightClickMenuRowComponent
+{
+  items: Item[] = [
+    { name: 'Item 1', value: 1 },
+    { name: 'Item 2', value: 2 },
+    { name: 'Item 3', value: 3 },
+    { name: 'Item 4', value: 4 },
+  ];
+
+  selectedItems: Item[] = []; // Array to hold selected items
+  menuPosition = { x: '0px', y: '0px' };
+  showMenuFlag = false;
+
+  toggleSelection(event: MouseEvent, item: Item)
+  {
+    if (event.ctrlKey)
+    {
+      // Toggle selection if Ctrl key is pressed
+      item.selected = !item.selected;
+    } else
+    {
+      // If Ctrl is not pressed, clear previous selection and select the current one
+      this.items.forEach(i => i.selected = false);
+      item.selected = true;
+    }
+
+    // Update selected items array
+    this.selectedItems = this.items.filter(i => i.selected);
+  }
+
+  showMenu(event: MouseEvent)
+  {
+    event.preventDefault(); // Prevent the default context menu
+    this.menuPosition = { x: `${event.clientX}px`, y: `${event.clientY}px` };
+    this.showMenuFlag = true;
+  }
+
+  hideMenu()
+  {
+    this.showMenuFlag = false;
+  }
+
+  performAction(action: string)
+  {
+    console.log(`Action performed: ${action} on`, this.selectedItems);
+    this.hideMenu();
+  }
+}
+interface Item
+{
+  name: string;
+  value: number;
+  selected?: boolean; // To track selection state
 }
